0cc1c662373b4cf1b604cdb58bb10dcb05af710b
kieshi48 <kieshi48@gmail.com>
2023-04-12 13:07:31 +0300

    Added PDF conversion

--- Complex/Form1.cs
+++ Complex/Form1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Forms;
 
 namespace Complex
@@ -10,6 +11,7 @@ namespace Complex
         }
 
         List<string> fileList = new List<string>();
+        bool exceptions = false;
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -24,6 +26,7 @@ namespace Complex
             openFileDialog1.Title = "ֲûבונטעו פאיכû Word ט Excel";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
+                fileList.Clear();
                 textBox1.Text = Path.GetDirectoryName(openFileDialog1.FileName);
                 foreach (string file in openFileDialog1.FileNames)
                 {
@@ -32,5 +35,39 @@ namespace Complex
                 MessageBox.Show($"Selected {fileList.Count} files");
             }
         }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (fileList.Count==0) throw new Exception("Please, select directory with Word files");
+
+                var wordList = from files in fileList where (files.EndsWith(".docx") || files.EndsWith(".doc")) select files;
+                
+                if (wordList.Count() == 0) throw new Exception("Please, select directory with Word files");
+
+                MessageBox.Show("Conversion procedure started");
+
+                var converterToPDF = new ConverterToPDF();
+
+                var task = converterToPDF.Converter(wordList);
+                await task;
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                exceptions = true;
+            }
+            finally
+            {
+                if (exceptions is false) 
+                {
+                    MessageBox.Show("All Word documents in the folder have been converted to PDF.");
+                }
+            }
+        }
+
     }
 }
