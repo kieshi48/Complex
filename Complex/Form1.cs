1df862fd2e56f47806f2fb20f36249cd1746571f
kieshi48 <kieshi48@gmail.com>
2023-04-12 17:44:06 +0300

    Add delete buttom

--- Complex/Form1.cs
+++ Complex/Form1.cs
@@ -15,15 +15,18 @@ namespace Complex
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            if (e.ColumnIndex == dataGridView1.Columns["Delete"].Index)
+            {
+                dataGridView1.Rows.RemoveAt(e.RowIndex);
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
             openFileDialog1.Multiselect = true;
             openFileDialog1.Filter = "Word & Excel Documents|*.doc;*.docx;*.xls;*.xlsx";
-            openFileDialog1.Title = "ֲûבונטעו פאיכû Word ט Excel";
+            openFileDialog1.Title = "Select Word and Excel";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 fileList.Clear();
@@ -36,7 +39,7 @@ namespace Complex
             }
         }
 
-        private async void button3_Click(object sender, EventArgs e)
+        private async void button3_Click_1(object sender, EventArgs e)
         {
             try
             {
@@ -74,12 +77,27 @@ namespace Complex
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click_1(object sender, EventArgs e)
         {
             var leftList = dataGridView1.Rows.Cast<DataGridViewRow>().Select(row => row.Cells[0].Value?.ToString()).ToList(); 
             var rightList = dataGridView1.Rows.Cast<DataGridViewRow>().Select(row => row.Cells[1].Value?.ToString()).ToList();
             
 
         }
+
+        //private void button1_Click_2(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void button3_Click_2(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void button2_Click_2(object sender, EventArgs e)
+        //{
+
+        //}
     }
 }
