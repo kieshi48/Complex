00ad7496233183eb9656cdc271079767ebe45037
kieshi48 <kieshi48@gmail.com>
2023-04-13 09:56:16 +0300

    Exclude zero row in data.txt file

--- Complex/Form1.Designer.cs
+++ Complex/Form1.Designer.cs
@@ -28,130 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.WhatEdit = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.EditTo = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.FolderPath = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            this.SuspendLayout();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            WhatEdit = new DataGridViewTextBoxColumn();
+            EditTo = new DataGridViewTextBoxColumn();
+            Delete = new DataGridViewButtonColumn();
+            FolderPath = new Label();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(29, 115);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 31);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Select files";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            button1.Location = new Point(25, 86);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Select files";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(29, 172);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(778, 31);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Bulk change rows in Word and Excel";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            button2.Location = new Point(25, 129);
+            button2.Name = "button2";
+            button2.Size = new Size(681, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Bulk change rows in Word and Excel";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 63);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(766, 27);
-            this.textBox1.TabIndex = 2;
+            textBox1.Location = new Point(25, 47);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(671, 23);
+            textBox1.TabIndex = 2;
             // 
             // dataGridView1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.WhatEdit,
-            this.EditTo,
-            this.Delete});
-            this.dataGridView1.Location = new System.Drawing.Point(29, 211);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.RowHeadersWidth = 100;
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(778, 164);
-            this.dataGridView1.TabIndex = 3;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { WhatEdit, EditTo, Delete });
+            dataGridView1.Location = new Point(25, 158);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 100;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(681, 177);
+            dataGridView1.TabIndex = 3;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // WhatEdit
             // 
-            this.WhatEdit.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.WhatEdit.HeaderText = "What edit";
-            this.WhatEdit.MinimumWidth = 6;
-            this.WhatEdit.Name = "WhatEdit";
+            WhatEdit.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            WhatEdit.HeaderText = "What edit";
+            WhatEdit.MinimumWidth = 6;
+            WhatEdit.Name = "WhatEdit";
             // 
             // EditTo
             // 
-            this.EditTo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.EditTo.HeaderText = "Edit to";
-            this.EditTo.MinimumWidth = 6;
-            this.EditTo.Name = "EditTo";
+            EditTo.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            EditTo.HeaderText = "Edit to";
+            EditTo.MinimumWidth = 6;
+            EditTo.Name = "EditTo";
             // 
             // Delete
             // 
-            this.Delete.HeaderText = "Delete";
-            this.Delete.MinimumWidth = 6;
-            this.Delete.Name = "Delete";
-            this.Delete.ReadOnly = true;
-            this.Delete.Text = "Delete";
-            this.Delete.UseColumnTextForButtonValue = true;
-            this.Delete.Width = 125;
+            Delete.HeaderText = "Delete";
+            Delete.MinimumWidth = 6;
+            Delete.Name = "Delete";
+            Delete.ReadOnly = true;
+            Delete.Text = "Delete";
+            Delete.UseColumnTextForButtonValue = true;
+            Delete.Width = 125;
             // 
             // FolderPath
             // 
-            this.FolderPath.AutoSize = true;
-            this.FolderPath.Location = new System.Drawing.Point(29, 31);
-            this.FolderPath.Name = "FolderPath";
-            this.FolderPath.Size = new System.Drawing.Size(85, 20);
-            this.FolderPath.TabIndex = 4;
-            this.FolderPath.Text = "Folder path";
+            FolderPath.AutoSize = true;
+            FolderPath.Location = new Point(25, 23);
+            FolderPath.Name = "FolderPath";
+            FolderPath.Size = new Size(67, 15);
+            FolderPath.TabIndex = 4;
+            FolderPath.Text = "Folder path";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(477, 115);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(319, 31);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Bulk convert from Word to PDF";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            button3.Location = new Point(417, 86);
+            button3.Name = "button3";
+            button3.Size = new Size(279, 23);
+            button3.TabIndex = 5;
+            button3.Text = "Bulk convert from Word to PDF";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click_1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(914, 600);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.FolderPath);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(FolderPath);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -162,9 +152,9 @@
         private DataGridView dataGridView1;
         private Label FolderPath;
         private Button button3;
+        private DataGridViewButtonColumn DeleteButtom;
         private DataGridViewTextBoxColumn WhatEdit;
         private DataGridViewTextBoxColumn EditTo;
-        private DataGridViewButtonColumn DeleteButtom;
         private DataGridViewButtonColumn Delete;
     }
 }
